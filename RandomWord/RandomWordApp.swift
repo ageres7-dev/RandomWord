e0679655946bcab535f514584c015b0374a9216e
Sergey Dolgikh <ageres7@gmail.com>
2022-07-02 23:23:17 +0500

    Initial Commit

---
+++ RandomWord/RandomWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomWordApp.swift
+//  RandomWord
+//
+//  Created by Sergey Dolgikh on 02.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct RandomWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
